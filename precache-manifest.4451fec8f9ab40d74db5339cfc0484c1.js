dc0ca04e7be866c177f6402286b213fbc5911fd7
unknown <monika.pikuzinska01@gmai.com>
2020-10-02 12:02:22 +0200

    Updates

---
+++ precache-manifest.4451fec8f9ab40d74db5339cfc0484c1.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35cd619c51523b98bc89459f0abfc423",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7a12d9d41acd6c7cbf95",
+    "url": "/portfolio/static/css/main.e9122aaa.chunk.css"
+  },
+  {
+    "revision": "a9f449a7375002de806a",
+    "url": "/portfolio/static/js/2.3905655d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.3905655d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a12d9d41acd6c7cbf95",
+    "url": "/portfolio/static/js/main.0dbf486d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "a1272ab6812df9488037eca35207ade1",
+    "url": "/portfolio/static/media/120090798_396326408430009_5887312141444674031_n.a1272ab6.jpg"
+  },
+  {
+    "revision": "12e14fbe5ecb15aeb462fe4975ba8444",
+    "url": "/portfolio/static/media/bookstore.12e14fbe.JPG"
+  },
+  {
+    "revision": "f421c9f64793ca7554d2e2fd16bb1728",
+    "url": "/portfolio/static/media/burger.f421c9f6.svg"
+  },
+  {
+    "revision": "b135172cd246b6e3b27af04827ae8bd7",
+    "url": "/portfolio/static/media/close.b135172c.svg"
+  },
+  {
+    "revision": "b32d6816e2831444818cd91c2d72ce48",
+    "url": "/portfolio/static/media/css-3.b32d6816.svg"
+  },
+  {
+    "revision": "8534ba1ea3b58ce9f83d379d1b11a366",
+    "url": "/portfolio/static/media/email.8534ba1e.svg"
+  },
+  {
+    "revision": "59cb7d5fac04b6f5e8944decdbaf5c30",
+    "url": "/portfolio/static/media/game.59cb7d5f.JPG"
+  },
+  {
+    "revision": "56e3a57f27a4c125a026366e85c7ecc8",
+    "url": "/portfolio/static/media/github.56e3a57f.svg"
+  },
+  {
+    "revision": "47224ef71e3cb621cad4c8cf01378e05",
+    "url": "/portfolio/static/media/html-5.47224ef7.svg"
+  },
+  {
+    "revision": "443cf54cb7d098d3c3e95c074b456caf",
+    "url": "/portfolio/static/media/javascript-1.443cf54c.svg"
+  },
+  {
+    "revision": "9b3d0c3ee6d5b31a462017f3e5577406",
+    "url": "/portfolio/static/media/linkedin.9b3d0c3e.svg"
+  },
+  {
+    "revision": "5bdf55cd5b62d7084e4010bad1b48d07",
+    "url": "/portfolio/static/media/marathon.5bdf55cd.JPG"
+  },
+  {
+    "revision": "0798aaf7db1009061cba5f43c87ee481",
+    "url": "/portfolio/static/media/quiz.0798aaf7.JPG"
+  },
+  {
+    "revision": "1297220c55e7c8f5ab26c8b164481e72",
+    "url": "/portfolio/static/media/react.1297220c.svg"
+  },
+  {
+    "revision": "224c67ae740e2b39cb41bb1c6ec04a15",
+    "url": "/portfolio/static/media/sass.224c67ae.svg"
+  }
+]);
